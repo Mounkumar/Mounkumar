4641a6ba4283acc63195959be59bf67a63c5db94
mounk <mounk@LAPTOP-IFNAH4LC>
2021-05-18 08:47:00 +0530

    first commit

---
+++ SpringBoot-OpenAPI-Generator/src/test/java/com/tools/SpringBootOpenAPIGenerator/SpringBootOpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.SpringBootOpenAPIGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOpenApiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
